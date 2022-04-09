4537dc46bf7cd2fd98d04b03ad1e20437ccbeb07
romanova <abdullah@pandatech.io>
2022-04-09 12:18:22 +0700

    invalid path

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-const main = () => {
-
+const fs = require('fs')
+const main = (dir) => {
+    return new Promise((resolve, reject) => {
+        fs.access(dir, (err) => {
+            if(err) {
+                reject({message: 'Invalid Path'})
+            }
+            else {
+            }
+        })
+    })
 };
 
 
